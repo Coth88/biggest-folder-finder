b2d21aee430c270460c5dfb9983468522e1d5486
Coth88 <coth88@gmail.com>
2022-12-13 12:35:41 +0500

    FolderSizeCalculator

--- biggest-folder-finder/src/Main.java
+++ biggest-folder-finder/src/Main.java
@@ -1,23 +1,16 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
+
+
         String folderPath = "C:\\Users\\coth8\\OneDrive\\Рабочий стол\\Student";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
-    }
-
-    public static long getFolderSize (File folder)
-    {
-        if (folder.isFile()) {
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for (File file: files ) {
-            sum += getFolderSize(file);
-        }
-        return sum;
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
     }
 }
